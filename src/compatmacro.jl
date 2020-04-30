1cb9be811c19429f9bd811bca98c11db18e55c75
Michael Abbott <me@escbook>
2020-04-30 13:14:15 +0200

    add A[begin] to compat macro

--- src/compatmacro.jl
+++ src/compatmacro.jl
@@ -3,16 +3,33 @@
 
 export @compat
 
-function _compat(ex::Expr)
+_compat(inref::Bool) = ex -> _compat(ex, inref)
+
+function _compat(ex::Expr, inref::Bool=false)
     if ex.head === :quote && isa(ex.args[1], Symbol)
         # Passthrough
         return ex
+    elseif ex.head === :ref
+        return Expr(:ref, _compat(ex.args[1]), map(_compat(true), ex.args[2:end])...)
     end
-    return Expr(ex.head, map(_compat, ex.args)...)
+    return Expr(ex.head, map(_compat(inref), ex.args)...)
 end
 
-_compat(ex) = ex
+function _compat(ex::Symbol, inref::Bool=false)
+    if ex === :begin && inref
+        return :($FirstIndex())
+    end
+    return ex
+end
+
+struct FirstIndex end
+
+Base.to_indices(A::AbstractArray, ax::Tuple, I::Tuple{FirstIndex, Vararg}) =
+    (first(first(ax)), Base.to_indices(A, Base.tail(ax), Base.tail(I))...)
+
+_compat(ex, inref::Bool=false) = ex
 
 macro compat(ex)
+    ex = macroexpand(__module__, ex)
     esc(_compat(ex))
 end
