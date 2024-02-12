114856fd3d99be0cee016974774c4c6edf214dd9
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2024-02-11 20:04:40 -0500

    Iterators.cycle(iter, n)

--- src/Compat.jl
+++ src/Compat.jl
@@ -795,6 +795,11 @@ if VERSION < v"1.9.0-DEV.461"
     Base.VersionNumber(v::VersionNumber) = v
 end
 
+# https://github.com/JuliaLang/julia/pull/47354/files
+if VERION < v"1.11-"
+    Iterators.cycle(xs, n::Integer) = Iterators.flatten(Iterators.repeated(xs, n))
+end
+
 include("deprecated.jl")
 
 end # module Compat
